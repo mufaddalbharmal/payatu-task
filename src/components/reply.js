ca3cb7c8cc10b6834e5ad96ad10546f4c484dfa3
Mufaddal Bharmal <mufaddalbharmal1074@gmail.com>
2024-05-25 14:50:20 +0530

    Final

--- src/components/reply.js
+++ src/components/reply.js
@@ -1,20 +1,31 @@
+import { useState } from "react";
+import { IoMdArrowDropright } from "react-icons/io"
+import { IoMdArrowDropdown } from "react-icons/io";
+
 const Reply = ({ author, text, replies }) => {
+    const [show, setShow] = useState(false)
+    const changeHandler = () => {
+        setShow(!show)
+    }
     return (
         <>
-            <div className="flex ">
+            <div className="flex select-none">
+                <span onClick={() => changeHandler()} className="text-2xl hover:cursor-pointer">
+                    {show ? <IoMdArrowDropdown /> : <IoMdArrowDropright />}
+                </span>
                 <img width={30} height={30} src={author?.avatar} />
                 <span className="ml-4 mt-1">{author?.name}</span>
             </div>
-            <div className="border-l-2 ml-4">
-                <div className="">
-                </div>
+            <div className={`${show ? "border-l-2" : ""} ml-4 `}>
                 <div className="pl-12">
                     {text}
-                    {
-                        replies && replies?.map((each) => (
-                            <Reply author={each?.author} text={each?.text} replies={each?.replies} />
-                        ))
-                    }
+                    <div>
+                        {
+                            show && replies?.map((each) => (
+                                <Reply author={each?.author} text={each?.text} replies={each?.replies} />
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </>
